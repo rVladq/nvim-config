5ce643c8fb22173f28f284172d896bab5076b71b
rVladq <vladruse7@gmail.com>
2025-04-09 22:44:25 +0300

    init

---
+++ lua/configs/gitsigns.lua
@@ -0,0 +1,44 @@
+local config = function()
+  require("gitsigns").setup {
+    on_attach = function(bufnr)
+      local gitsigns = require('gitsigns')
+      local wk = require("which-key")
+
+      local function map(mode, l, r, desc)
+        wk.add({
+          { l, r, desc = desc or "", mode = mode } -- Use a description if available, or an empty string
+        })
+      end
+
+      -- Actions
+
+      --stage
+      map('n', '<leader>gs', gitsigns.stage_hunk, "stage_hunk")
+      map('n', '<leader>gr', gitsigns.reset_hunk, "reset_hunk")
+
+      map('n', '<leader>gS', gitsigns.stage_buffer, "stage_buffer")
+      map('n', '<leader>gR', gitsigns.reset_buffer, "reset_buffer")
+
+      --diff
+      map('n', '<leader>gd', function()
+        gitsigns.diffthis('~')
+      end, "diff_head")
+      map('n', '<leader>gD', gitsigns.diffthis, "diff_index")
+
+      map('n', '<leader>gp', gitsigns.preview_hunk, "diff_hunk")
+      map('n', '<leader>gP', gitsigns.preview_hunk_inline, "diff_hunk_inline")
+
+      --blame
+      map('n', '<leader>gb', function()
+        gitsigns.blame_line({ full = true })
+      end, "blame")
+
+      -- Toggles
+      map('n', '<leader>tb', gitsigns.toggle_current_line_blame, "toggle_current_line_blame")
+      map('n', '<leader>td', gitsigns.toggle_deleted, "toggle_deleted")
+      map('n', '<leader>tw', gitsigns.toggle_word_diff, "toggle_word_diff")
+    end
+  }
+end
+
+return config
