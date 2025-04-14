8884a04aa04a76a4e748c8de40d6c49fa039a889
rVladq <vladruse7@gmail.com>
2025-04-14 22:54:26 +0300

    Add auto-session config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -34,9 +34,6 @@ return {
     'rmagatti/auto-session',
     lazy = false,
 
-    opts = {
-      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-      -- log_level = 'debug',
-    }
-  }
+    opts = require("configs.auto-session")
+  },
 }
