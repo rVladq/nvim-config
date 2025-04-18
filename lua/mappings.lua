47c54303f864b9ed4d04b22b665d8e45972b1837
rVladq <vladruse7@gmail.com>
2025-04-18 21:51:50 +0300

    Add brighter highlight && Add code_action mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,5 +8,6 @@ map('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
 map('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
 
 map('n', '<C-k>', vim.diagnostic.open_float)
+map('n', '<leader>ca', vim.lsp.buf.code_action)
 
 map('i', '<C-c>', '<ESC>')
--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,9 @@ vim.o.cursorlineopt = 'both' -- to enable cursorline!
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.clipboard = "unnamedplus"
+
+vim.api.nvim_set_hl(0, "Visual", {
+  bg = "#ff69b4", -- bright cyan
+  fg = "#000000",
+  bold = true,
+})
