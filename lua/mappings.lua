d0af6337445cc4c1aa87ece9c861a902de4fe45e
rVladq <vladruse7@gmail.com>
2025-04-19 05:38:59 +0300

    .

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,6 +7,6 @@ map('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
 
 map('n', '<C-k>', vim.diagnostic.open_float)
 map('n', '<leader>ca', vim.lsp.buf.code_action)
-map('n', '<C-K>', vim.lsp.buf.signature_help)
+-- map('n', '<C-K>', vim.lsp.buf.signature_help)
 
 map('i', '<C-c>', '<ESC>')
