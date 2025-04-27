a94afcfe7e538c369bc0171166af5c6a363d4df3
rVladq <vladruse7@gmail.com>
2025-04-28 00:36:36 +0300

    sum changes

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -18,7 +18,36 @@ vim.diagnostic.config({
   update_in_insert = true,
 })
 
+local orig_virtual_text = vim.diagnostic.handlers.virtual_text
+
+vim.diagnostic.handlers.virtual_text = {
+  show = function(namespace, bufnr, diagnostics, opts)
+    local filtered_diags = {}
+    for _, diag in ipairs(diagnostics) do
+      if diag.severity ~= vim.diagnostic.severity.WARN then
+        table.insert(filtered_diags, diag)
+      end
+    end
+    orig_virtual_text.show(namespace, bufnr, filtered_diags, opts)
+  end,
+  hide = function(namespace, bufnr)
+    orig_virtual_text.hide(namespace, bufnr)
+  end,
+}
+
 vim.keymap.set('n', '<leader>R', vim.lsp.buf.rename)
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {
+    border = "rounded"
+  }
+)
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    border = "rounded" -- or "single", "double", "shadow", etc
+  }
+)
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
